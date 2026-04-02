600241117322093d14f5cff2c7aa3c48ab91bb76
n2w-rc <127403802+n2w-rc@users.noreply.github.com>
2026-04-02 16:40:45 +0800

    Try?

--- home/home.nix
+++ home/home.nix
@@ -13,18 +13,45 @@
     gemini-cli
     nixfmt
     antigravity
+    starship
+    eza
   ];
 
   programs.caelestia-dots = {
     enable = true;
     hypr.enable = true;
     editor.enable = true;
-    term.enable = true;
+    term.enable = false;
     btop.enable = true;
     foot.enable = true;
   };
 
-  programs.starship.enable = true;
+# starship.toml from upstream caelestia-dots
+  xdg.configFile."starship.toml".source =
+    "${inputs.caelestia-nixos.inputs.caelestia-dots}/starship/starship.toml";
+  
+  # fish: the upstream config is literally just these two things
+  programs.fish = {
+    enable = true;
+    interactiveShellInit = ''
+      starship init fish | source
+    '';
+    functions = {
+      mark_prompt_start = {
+        onEvent = "fish_prompt";
+        body = ''echo -en "\e]133;A\e\\"'';
+      };
+    };
+  };
+  
+  # eza aliases (standard ones caelestia-nixos generates)
+  programs.fish.shellAliases = {
+    ls  = "eza";
+    ll  = "eza -lh";
+    la  = "eza -lah";
+    lt  = "eza --tree";
+    # add more as desired
+  };
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
