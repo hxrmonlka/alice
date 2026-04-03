f97e2e198d024d1ae95593a9445f13144255f867
n2w-rc <127403802+n2w-rc@users.noreply.github.com>
2026-04-03 09:08:18 +0800

    Whatever.

--- home/modules/programs/starship.nix
+++ home/modules/programs/starship.nix
@@ -1,35 +1,10 @@
 { ... }: {
   programs.starship = {
     enable = true;
-    enableFishIntegration = true; # For fish shell
+    enableFishIntegration = true;
     settings = {
       format = ''
-        [](#9A348E)\
-        $os\
-        $username\
-        [](bg:#DA627D fg:#9A348E)\
-        $directory\
-        [](fg:#DA627D bg:#FCA17D)\
-        $git_branch\
-        $git_status\
-        [](fg:#FCA17D bg:#86BBD8)\
-        $c\
-        $elixir\
-        $elm\
-        $golang\
-        $gradle\
-        $haskell\
-        $java\
-        $julia\
-        $nodejs\
-        $nim\
-        $rust\
-        $scala\
-        [](fg:#86BBD8 bg:#06969A)\
-        $docker_context\
-        [](fg:#06969A bg:#33658A)\
-        $time\
-        [ ](fg:#33658A)\
+        [](#9A348E)$os$username[](bg:#DA627D fg:#9A348E)$directory[](fg:#DA627D bg:#FCA17D)$git_branch$git_status[](fg:#FCA17D bg:#86BBD8)$c$elixir$elm$golang$gradle$haskell$java$julia$nodejs$nim$rust$scala[](fg:#86BBD8 bg:#06969A)$docker_context[](fg:#06969A bg:#33658A)$time[ ](fg:#33658A)
       '';
       
       username = {
@@ -53,28 +28,40 @@
         };
       };
       
-      c.symbol = " ";
-      c.style = "bg:#86BBD8";
-      c.format = "[ $symbol ($version) ]($style)";
+      c = {
+        symbol = " ";
+        style = "bg:#86BBD8";
+        format = "[ $symbol ($version) ]($style)";
+      };
       
-      git_branch.symbol = "";
-      git_branch.style = "bg:#FCA17D";
-      git_branch.format = "[ $symbol $branch ]($style)";
+      git_branch = {
+        symbol = "";
+        style = "bg:#FCA17D";
+        format = "[ $symbol $branch ]($style)";
+      };
       
-      git_status.style = "bg:#FCA17D";
-      git_status.format = "[$all_status$ahead_behind ]($style)";
+      git_status = {
+        style = "bg:#FCA17D";
+        format = "[$all_status$ahead_behind ]($style)";
+      };
       
-      nodejs.symbol = "";
-      nodejs.style = "bg:#86BBD8";
-      nodejs.format = "[ $symbol ($version) ]($style)";
+      nodejs = {
+        symbol = "";
+        style = "bg:#86BBD8";
+        format = "[ $symbol ($version) ]($style)";
+      };
       
-      rust.symbol = "";
-      rust.style = "bg:#86BBD8";
-      rust.format = "[ $symbol ($version) ]($style)";
+      rust = {
+        symbol = "";
+        style = "bg:#86BBD8";
+        format = "[ $symbol ($version) ]($style)";
+      };
       
-      docker_context.symbol = " ";
-      docker_context.style = "bg:#06969A";
-      docker_context.format = "[ $symbol $context ]($style)";
+      docker_context = {
+        symbol = " ";
+        style = "bg:#06969A";
+        format = "[ $symbol $context ]($style)";
+      };
       
       time = {
         disabled = false;
