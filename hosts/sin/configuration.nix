1007fec6b6c1da3922657b4e740016d119920713
yixanni <d.nightcore.ee1@gmail.com>
2026-04-04 21:44:08 +0800

    build: change default user shell to zsh

--- hosts/sin/configuration.nix
+++ hosts/sin/configuration.nix
@@ -161,6 +161,7 @@
   #   enableSSHSupport = true;
   # };
 
+  environment.shells = [ pkgs.zsh ];
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
