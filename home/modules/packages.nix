d3d7b186a4ae83b20c366918033d73f46fcc7de6
yixanni <d.nightcore.ee1@gmail.com>
2026-04-06 20:05:02 +0800

    build: add cliphist to pkgs

--- home/modules/packages.nix
+++ home/modules/packages.nix
@@ -1,4 +1,5 @@
-{ pkgs, ... }: {
+{ pkgs, ... }:
+{
   home.packages = with pkgs; [
     gemini-cli
     nixfmt
@@ -21,5 +22,6 @@
     fish-lsp
     babelfish
     github-desktop
-  ]; 
+    cliphist
+  ];
 }
