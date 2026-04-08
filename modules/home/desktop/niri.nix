03bb591d73b527ccdd747e5daaca2d093b5b3585
yixanni <d.nightcore.ee1@gmail.com>
2026-04-08 22:00:34 +0800

    i'll do it

--- modules/home/desktop/niri.nix
+++ modules/home/desktop/niri.nix
@@ -88,22 +88,22 @@
           };
           animations = {
             slowdown = 0.8;
-            workspace-switch.kind.spring = {
+            workspace-switch.spring = {
               damping-ratio = 1.0;
               stiffness = 800;
               epsilon = 0.0001;
             };
-            window-open.kind.spring = {
+            window-open.spring = {
               damping-ratio = 0.85;
               stiffness = 600;
               epsilon = 0.0001;
             };
-            window-close.kind.spring = {
+            window-close.spring = {
               damping-ratio = 0.85;
               stiffness = 600;
               epsilon = 0.0001;
             };
-            horizontal-view-movement.kind.spring = {
+            horizontal-view-movement.spring = {
               damping-ratio = 1.0;
               stiffness = 800;
               epsilon = 0.0001;
@@ -111,101 +111,96 @@
           };
           window-rules = [
             {
-              geometry-corner-radius = {
-                top-left = 8.0;
-                top-right = 8.0;
-                bottom-left = 8.0;
-                bottom-right = 8.0;
-              };
+              geometry-corner-radius = 8.0;
               clip-to-geometry = true;
             }
           ];
           binds = {
-            "Mod+Return".action.spawn-sh = lib.getExe pkgs.kitty;
-            "Mod+Q".action.close-window = { };
-            "Mod+F".action.maximize-column = { };
-            "Mod+G".action.fullscreen-window = { };
-            "Mod+Shift+F".action.toggle-window-floating = { };
-            "Mod+C".action.center-column = { };
+            "Mod+Return".spawn-sh = lib.getExe pkgs.kitty;
+            "Mod+Q".close-window = { };
+            "Mod+F".maximize-column = { };
+            "Mod+G".fullscreen-window = { };
+            "Mod+Shift+F".toggle-window-floating = { };
+            "Mod+C".center-column = { };
 
-            "Mod+H".action.focus-column-left = { };
-            "Mod+L".action.focus-column-right = { };
-            "Mod+K".action.focus-window-up = { };
-            "Mod+J".action.focus-window-down = { };
+            "Mod+H".focus-column-left = { };
+            "Mod+L".focus-column-right = { };
+            "Mod+K".focus-window-up = { };
+            "Mod+J".focus-window-down = { };
 
-            "Mod+Left".action.focus-column-left = { };
-            "Mod+Right".action.focus-column-right = { };
-            "Mod+Up".action.focus-window-up = { };
-            "Mod+Down".action.focus-window-down = { };
+            "Mod+Left".focus-column-left = { };
+            "Mod+Right".focus-column-right = { };
+            "Mod+Up".focus-window-up = { };
+            "Mod+Down".focus-window-down = { };
 
-            "Mod+Shift+H".action.move-column-left = { };
-            "Mod+Shift+L".action.move-column-right = { };
-            "Mod+Shift+K".action.move-window-up = { };
-            "Mod+Shift+J".action.move-window-down = { };
+            "Mod+Shift+H".move-column-left = { };
+            "Mod+Shift+L".move-column-right = { };
+            "Mod+Shift+K".move-window-up = { };
+            "Mod+Shift+J".move-window-down = { };
 
-            "Mod+Ctrl+H".action.focus-monitor-left = { };
-            "Mod+Ctrl+L".action.focus-monitor-right = { };
+            "Mod+Ctrl+H".focus-monitor-left = { };
+            "Mod+Ctrl+L".focus-monitor-right = { };
 
-            "Mod+1".action.focus-workspace = 1;
-            "Mod+2".action.focus-workspace = 2;
-            "Mod+3".action.focus-workspace = 3;
-            "Mod+4".action.focus-workspace = 4;
-            "Mod+5".action.focus-workspace = 5;
-            "Mod+6".action.focus-workspace = 6;
-            "Mod+7".action.focus-workspace = 7;
-            "Mod+8".action.focus-workspace = 8;
-            "Mod+9".action.focus-workspace = 9;
+            "Mod+1".focus-workspace = 1;
+            "Mod+2".focus-workspace = 2;
+            "Mod+3".focus-workspace = 3;
+            "Mod+4".focus-workspace = 4;
+            "Mod+5".focus-workspace = 5;
+            "Mod+6".focus-workspace = 6;
+            "Mod+7".focus-workspace = 7;
+            "Mod+8".focus-workspace = 8;
+            "Mod+9".focus-workspace = 9;
 
-            "Mod+Shift+1".action.move-column-to-workspace = 1;
-            "Mod+Shift+2".action.move-column-to-workspace = 2;
-            "Mod+Shift+3".action.move-column-to-workspace = 3;
-            "Mod+Shift+4".action.move-column-to-workspace = 4;
-            "Mod+Shift+5".action.move-column-to-workspace = 5;
-            "Mod+Shift+6".action.move-column-to-workspace = 6;
-            "Mod+Shift+7".action.move-column-to-workspace = 7;
-            "Mod+Shift+8".action.move-column-to-workspace = 8;
-            "Mod+Shift+9".action.move-column-to-workspace = 9;
+            "Mod+Shift+1".move-column-to-workspace = 1;
+            "Mod+Shift+2".move-column-to-workspace = 2;
+            "Mod+Shift+3".move-column-to-workspace = 3;
+            "Mod+Shift+4".move-column-to-workspace = 4;
+            "Mod+Shift+5".move-column-to-workspace = 5;
+            "Mod+Shift+6".move-column-to-workspace = 6;
+            "Mod+Shift+7".move-column-to-workspace = 7;
+            "Mod+Shift+8".move-column-to-workspace = 8;
+            "Mod+Shift+9".move-column-to-workspace = 9;
 
-            "Mod+Comma".action.consume-window-into-column = { };
-            "Mod+Period".action.expel-window-from-column = { };
+            "Mod+Comma".consume-window-into-column = { };
+            "Mod+Period".expel-window-from-column = { };
 
-            "Mod+R".action.switch-preset-column-width = { };
-            "Mod+Shift+R".action.reset-window-height = { };
+            "Mod+R".switch-preset-column-width = { };
+            "Mod+Shift+R".reset-window-height = { };
 
-            "Mod+Minus".action.set-column-width = "-10%";
-            "Mod+Equal".action.set-column-width = "+10%";
-            "Mod+Shift+Minus".action.set-window-height = "-10%";
-            "Mod+Shift+Equal".action.set-window-height = "+10%";
+            "Mod+Minus".set-column-width = "-10%";
+            "Mod+Equal".set-column-width = "+10%";
+            "Mod+Shift+Minus".set-window-height = "-10%";
+            "Mod+Shift+Equal".set-window-height = "+10%";
 
-            "Print".action.screenshot = { };
-            "Mod+Print".action.screenshot-screen = { };
-            "Mod+Shift+Print".action.screenshot-window = { };
+            "Print".screenshot = { };
+            "Mod+Print".screenshot-screen = { };
+            "Mod+Shift+Print".screenshot-window = { };
 
-            "XF86AudioRaiseVolume".action.spawn = [
+            "XF86AudioRaiseVolume".spawn = [
               "wpctl"
               "set-volume"
               "@DEFAULT_AUDIO_SINK@"
               "0.1+"
             ];
-            "XF86AudioLowerVolume".action.spawn = [
+            "XF86AudioLowerVolume".spawn = [
               "wpctl"
               "set-volume"
               "@DEFAULT_AUDIO_SINK@"
               "0.1-"
             ];
-            "XF86AudioMute".action.spawn = [
+            "XF86AudioMute".spawn = [
               "wpctl"
               "set-mute"
               "@DEFAULT_AUDIO_SINK@"
               "toggle"
             ];
-            "XF86MonBrightnessUp".action.spawn-sh = "${lib.getExe pkgs.brightnessctl} s 5%+";
-            "XF86MonBrightnessDown".action.spawn-sh = "${lib.getExe pkgs.brightnessctl} s 5%-";
-            "Mod+S".action.spawn = "${lib.getExe self'.packages.aliceNoctalia} ipc call launcher toggle";
-            "Mod+D".action.spawn-sh = lib.getExe pkgs.wlr-which-key;
+            "XF86MonBrightnessUp".spawn-sh = "${lib.getExe pkgs.brightnessctl} s 5%+";
+            "XF86MonBrightnessDown".spawn-sh = "${lib.getExe pkgs.brightnessctl} s 5%-";
+            "Mod+S".spawn = "${lib.getExe self'.packages.aliceNoctalia} ipc call launcher toggle";
+            "Mod+D".spawn-sh = lib.getExe pkgs.wlr-which-key;
 
-            "Mod+Shift+E".action.quit.skip-confirmation = true;
-            "Mod+Shift+P".action.power-off-monitors = { };
+            "Mod+Shift+E".quit.skip-confirmation = true;
+            "Mod+Shift+P".power-off-monitors = { };
           };
         };
       };
