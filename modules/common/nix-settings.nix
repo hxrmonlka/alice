acb2504fd82e1b4e49f68090b033648637ba2f5b
hxrmonlka <127403802+hxrmonlka@users.noreply.github.com>
2026-06-30 22:01:22 +0800

    permit insecure package.

--- modules/common/nix-settings.nix
+++ modules/common/nix-settings.nix
@@ -5,7 +5,12 @@
 }: {
   flake.custom.commonModules.nixSettings = {pkgs, ...}: {
     nixpkgs = {
-      config.allowUnfree = true;
+      config = {
+        allowUnfree = true;
+        permittedInsecurePackages = [
+          "electron-39.8.10"
+        ];
+      };
       overlays = [
         inputs.nix-cachyos-kernel.overlays.pinned
         inputs.rust-overlay.overlays.default
