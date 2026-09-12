751649c7f53f25fe61bdeff21f02e27f62cc0fee
Ota <127403802+hxrmonlka@users.noreply.github.com>
2026-09-12 19:52:40 +0800

    feat(serpentine): sync Millennium Material theme with matugen

--- modules/hosts/serpentine/settings/gaming-override.nix
+++ modules/hosts/serpentine/settings/gaming-override.nix
@@ -9,6 +9,29 @@
     ...
   }: {
     imports = [self.nixosModules.gamingSettings];
+
     programs.steam.package = lib.mkForce pkgs.millennium-steam;
+
+    home-manager.users.alice = {
+      xdg.configFile = {
+        "matugen/millennium.css".text = ''
+          :root {
+              --theme-color: "Matugen";
+              --hue-rotate: 220deg;
+              <* for name, value in colors *>
+              --md-sys-color-{{name | replace: "_", "-" }}: {{value.default.rgb}};
+              <* endfor *>
+          }
+        '';
+
+        "matugen/config.toml".text = ''
+          [config]
+
+          [templates.millennium]
+          input_path = '/home/alice/.config/matugen/millennium.css'
+          output_path = '~/.steam/steam/millennium/themes/Material-Theme/css/main/colors/matugen.css'
+        '';
+      };
+    };
   };
 }
