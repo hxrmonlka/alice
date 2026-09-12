fc9af62b05c7837ec388650ff9bace1705e19967
Ota <127403802+hxrmonlka@users.noreply.github.com>
2026-09-12 20:53:36 +0800

    refactor(starship): move Matugen configuration to dedicated module

--- modules/home/alice/programs/starship.nix
+++ modules/home/alice/programs/starship.nix
@@ -12,17 +12,4 @@
       enableBashIntegration = true;
     };
   };
-
-  flake.custom.alice.starshipMatugen = {config, ...}: {
-    xdg.configFile."matugen/templates/starship.toml".source = ./misc/starship-matugen.toml;
-    xdg.configFile."matugen/config.toml".text = ''
-      [config]
-
-      [templates]
-
-      [templates.starship]
-      input_path = '${config.xdg.configHome}/matugen/templates/starship.toml'
-      output_path = '${config.home.homeDirectory}/.config/starship.toml'
-    '';
-  };
 }
