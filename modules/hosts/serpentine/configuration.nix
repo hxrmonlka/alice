4e2638d39aa903ee9a52ad00d37d29c500e19cfa
Ota <127403802+hxrmonlka@users.noreply.github.com>
2026-09-15 18:06:58 +0800

    feat: enable declarative Helium extensions

--- modules/hosts/serpentine/configuration.nix
+++ modules/hosts/serpentine/configuration.nix
@@ -10,6 +10,7 @@
       inputs.nix-flatpak.nixosModules.nix-flatpak
       inputs.mangowc.nixosModules.mango
       inputs.lumina.nixosModules.signature
+      inputs.lumina.nixosModules.helium-extensions
 
       # Self section (under ./modules/hosts/serpentine/)
       self.custom.serpentine.networking
@@ -40,6 +41,10 @@
       # Misc
       self.nixosModules.dgreet
       self.nixosModules.mangoWC
+    ];
+
+    lumina.helium.extensions = [
+      "cofdbpoegempjloogbagkncekinflcnj"
     ];
 
     home-manager = {
