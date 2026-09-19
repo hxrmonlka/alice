d09e5820a78f7e40975b32ce429569a562103057
Ota <127403802+hxrmonlka@users.noreply.github.com>
2026-09-19 12:09:22 +0800

    i have negative 10000 aura.

--- modules/hosts/tanuki/settings/services-override.nix
+++ modules/hosts/tanuki/settings/services-override.nix
@@ -3,12 +3,24 @@
   inputs,
   ...
 }: {
-  flake.custom.tanuki.services-override = {...}: {
+  flake.custom.tanuki.services-override = {pkgs, ...}: {
     imports = [self.custom.commonModules.systemServices];
 
     services = {
       displayManager.gdm.enable = true;
       desktopManager.gnome.enable = true;
+    };
+
+    systemd.services.aura-static-color = {
+      description = "Set ASUS Aura keyboard backlight to a static color";
+      after = ["asusd.service"];
+      requires = ["asusd.service"];
+      wantedBy = ["multi-user.target"];
+      serviceConfig = {
+        type = "oneshot";
+        RemainAfterExit = true;
+        ExecStart = "${pkgs.asusctl}/bin/asusctl aura static -c 00CCFF";
+      };
     };
   };
 }
